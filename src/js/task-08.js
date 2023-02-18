e7139fda8b1b451d12897189664856c4963de0a6
DagmaraSobczak <daga.sobczak@gmail.com>
2023-02-18 22:03:44 +0100

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { login, password },
+  } = event.currentTarget;
+
+  if (login.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
