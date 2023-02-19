84cec38ef10b2db8f01b8284b213d870f20d538b
DagmaraSobczak <daga.sobczak@gmail.com>
2023-02-19 12:10:44 +0100

    task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const button = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+button.addEventListener("click", function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+});
