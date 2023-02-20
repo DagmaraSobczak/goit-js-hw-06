98a23e1b59d480e6612aba12b5a996a26b29de72
DagmaraSobczak <daga.sobczak@gmail.com>
2023-02-20 08:08:07 +0100

    poprawka 2

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector("#validation-input");
 textInput.addEventListener("blur", () => {
   const length = textInput.getAttribute("data-length");
-  if (textInput.value.length === length) {
+  if (textInput.value.length === Number(length)) {
     textInput.classList.add("valid");
     textInput.classList.remove("invalid");
   } else {
